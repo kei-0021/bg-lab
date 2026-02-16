40827b453c5659ba79f686167927923f1fffc4e6
kei-0021 <quita02@gmail.com>
2026-02-16 21:03:35 +0000

    issue #15 イベントの削除漏れがあったので消しました

--- public/data/customEvents.js
+++ public/data/customEvents.js
@@ -8,21 +8,5 @@ export function customEvents() {
       socket.to(roomId).emit("reset:draggable");
       socket.emit("reset:draggable");
     },
-
-    // // ★ カーソル同期のイベントハンドラーを追加
-    // "cursor:move": (socket, data) => {
-    //   const { roomId, x, y } = data;
-
-    //   // 送信元ソケットのIDを取得
-    //   const playerId = socket.id;
-
-    //   // ルーム内の他のクライアントにカーソル座標をブロードキャスト
-    //   // クライアント側で 'cursor:update' イベントとして受信されます
-    //   socket.to(roomId).emit("cursor:update", {
-    //     playerId, // 誰のカーソルか識別するためにIDを含める
-    //     x, // 画面のX座標 (clientX)
-    //     y, // 画面のY座標 (clientY)
-    //   });
-    // },
   };
 }
